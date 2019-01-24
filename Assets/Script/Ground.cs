515dcac4d8da1cee28b2a5d6d01b462089e192d3
Nhan <dangnhan843@email.arizona.edu>
2019-01-24 14:49:53 -0700

    init1

---
+++ Assets/Script/Ground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    public float speed = 5f;
+    Renderer rend;
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * speed;
+        rend.material.mainTextureOffset = new Vector2(offset, 0);
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    rb.useGravity = false;
+    //}
+    public float ForwardForce = 2000f;
+    public float SlideForce =500f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // force to go forward
+        rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("d"))
+        {
+            rb.AddForce(SlideForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-SlideForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
